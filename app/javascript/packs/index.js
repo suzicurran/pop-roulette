f3a595d10de5148886722f1e51631de973ffc2cb
Suzi Curran <suzicurran@Lux.local>
2017-07-22 12:05:53 -0400

    Setup rails, react, testing environment

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<div>Hello</div>, document.getElementById('app'));
+})
